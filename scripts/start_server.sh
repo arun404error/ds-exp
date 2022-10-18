5f2e7c067ee941e4af9514954fbebfcbb295122f
arunkumarc <arun.c@gdn-commerce.com>
2022-10-18 16:25:38 +0530

    installing annoy in docker

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,6 +2,6 @@ python scripts/consul_fetch.py
 echo 'fetched config folder from consul'
 
 #poetry run
-newrelic-admin run-program gunicorn -k uvicorn.workers.UvicornWorker src.main:app --config 'configs/gunicorn_conf.py'
+newrelic-admin run-program gunicorn --config 'configs/gunicorn_conf.py' src.main:app
 
 #poetry run ucivorn src.main:app --reload --port 8080
