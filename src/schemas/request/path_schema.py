6f7a0bfc13d14d373ee4b7fb2921eb69b3e8f947
arunkumarc <arun.c@gdn-commerce.com>
2022-11-23 18:57:43 +0530

    response schema changed

--- src/schemas/request/path_schema.py
+++ src/schemas/request/path_schema.py
@@ -4,3 +4,9 @@ class PathSchema(BaseModel):
     path:constr(min_length=2,max_length=800)
     class Config:
         extra="forbid"
+
+class Base64Schema(BaseModel):
+    base64_string:constr(min_length=2)
+
+    class Config:
+        extra="forbid"
