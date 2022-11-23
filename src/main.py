b6d3df98f0f8a13e10275f54c36db39d15d0b1a1
arunkumarc <arun.c@gdn-commerce.com>
2022-11-23 18:25:18 +0530

    google cloud logging disabled

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI
 from src.routers import sys_info,prediction_route
 import logging
-from src.logging.configure_logging import configure_logging
+# from src.logging.configure_logging import configure_logging
 
 app=FastAPI()
 
 app.include_router(sys_info.router)
 app.include_router(prediction_route.router)
 
-@app.on_event("startup")
-def on_startup():
-    # configure logging
-    configure_logging("fast-api worker")
-    logging.info("google cloud logging is configured successfully")
+# @app.on_event("startup")
+# def on_startup():
+#     # configure logging
+#     configure_logging("fast-api worker")
+#     logging.info("google cloud logging is configured successfully")
